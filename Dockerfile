aa04e7c0ccb0331efa15b72a5eb2afdfa7e0ec55
Alex Fleming <alex@alexfleming.dev>
2020-01-29 21:21:47 +0000

    Finish off presentation

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM node:10-alpine
 WORKDIR /app/presentation/
 COPY . .
 RUN npm install
+RUN chmod +x another-slide.sh
 ENTRYPOINT [ "npm", "start" ]
---
+++ another-slide.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+
+echo "## A generated slide" >> another-slide.md
+echo "- $HOSTNAME" >> another-slide.md
+echo "- $A_PARAM" >> another-slide.md
+echo "- $SECRET_PASSWORD" >> another-slide.md
